7f40266a4e660c4c8278cac40cb37fb4f67b92bf
Timothy Pokanai <pokanait@mcmaster.ca>
2024-10-14 02:53:08 -0400

    Initial commit for SwifTune

---
+++ SwifTune/SwifTuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifTuneApp.swift
+//  SwifTune
+//
+//  Created by Tim Pokanai on 2024-10-14.
+//
+
+import SwiftUI
+
+@main
+struct SwifTuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwifTuneTests/SwifTuneTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwifTuneTests.swift
+//  SwifTuneTests
+//
+//  Created by Tim Pokanai on 2024-10-14.
+//
+
+import Testing
+
+struct SwifTuneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
